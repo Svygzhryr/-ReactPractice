261dc3f0a368e653c7051f1dc52448e0724cb125
Svygzhryr <pamixy@gmail.com>
2022-12-06 11:12:34 +0300

    for the machine is immortal

--- src/components/Approuter.jsx
+++ src/components/Approuter.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import {  Navigate, Route, Routes } from 'react-router-dom';
-import About from '../pages/About';
-import Posts from '../pages/Posts'
-import Error from '../pages/Error';
-import PostIdPage from '../pages/PostIdPage';
+import {routes} from '../router/routes'
 
 export default function Approuter() {
   return (
     <Routes>
-        <Route path='/about' element={<About/>}></Route>
-        <Route exact path='/posts' element={<Posts/>}></Route>
-        <Route path='/error' element={<Error/>}></Route>
-        <Route exact path='/posts/:id' element={<PostIdPage/>}></Route>
-        <Route
-        path="*"
-        element={<Navigate to="/error" replace />}
-         />
-     </Routes>
+        {routes.map((routes, index) => 
+          <Route key={index} element={<routes.Element/>} path={routes.path}/>
+        )}    
+    </Routes>
   )
 }
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,9 @@
 import About from "../pages/About";
+import PostIdPage from "../pages/PostIdPage";
+import Posts from "../pages/Posts";
 
 export const routes = [
-    {path: '/about', component: About, exact: true}
+    {path: '/about', Element: About},
+    {path: '/posts', Element: Posts},
+    {path: '/posts/:id', Element: PostIdPage},
 ]
