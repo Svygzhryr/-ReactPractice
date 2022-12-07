3cc7c44d731169895f070f50f26fc0c39f0b4ff8
Svygzhryr <pamixy@gmail.com>
2022-12-07 09:42:26 +0300

    For the machine is immortal.

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,6 +1,11 @@
 import '../css/App.css';
-import { useState, useEffect } from 'react';
+import '../css/Posts.css'
+import { useState, useEffect, useRef } from 'react';
 import { usePosts } from '../hooks/usePosts';
+import { useFetching } from '../hooks/useFetching';
+import { getPagesArray } from '../utils/pages';
+import { getPageCount } from '../utils/pages';
+import { useObserver } from '../hooks/useObserver';
 import PostList from '../components/PostList';
 import PostForm from '../components/PostForm';
 import PostFilter from '../components/PostFilter';
@@ -8,10 +13,9 @@ import MyModal from '../UI/MyModal/MyModal';
 import MyButton from '../UI/button/MyButton'
 import PostService from '../API/PostService';
 import Loader from '../UI/Loader/Loader';
-import { useFetching } from '../hooks/useFetching';
-import { getPagesArray } from '../utils/pages';
-import { getPageCount } from '../utils/pages';
+import MySelect from '../UI/select/MySelect';
 import Pagination from '../UI/pagination/Pagination';
+
 
 function Posts() {
 
@@ -22,8 +26,10 @@ function Posts() {
   const [totalPages, setTotalPages] = useState(0);
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
+  const lastElement = useRef();
 
   let pagesArray = getPagesArray(totalPages);
+
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -32,7 +38,7 @@ function Posts() {
 
   const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
+    setPosts([...posts, ...response.data]);
     const totalCount = response.headers['x-total-count']
     setTotalPages(getPageCount(totalCount, limit))
   })
@@ -42,12 +48,16 @@ function Posts() {
   }
 
   useEffect(() => {
-    fetchPosts();
-  }, [page])
+    fetchPosts(limit, page);
+  }, [page, limit])
 
   const changePage = (page) => {
     setPage(page);
   }
+
+  useObserver(lastElement, page < totalPages, isPostLoading, () => {
+    setPage(page + 1);
+  })
 
 
   return (
@@ -62,12 +72,24 @@ function Posts() {
       filter={filter} 
       setFilter={setFilter}
       />
+      <MySelect 
+      className='maxPosts'
+      value={limit} 
+      onChange={value => setLimit(value)} 
+      defaultValue="Кол-во элементов на странице" 
+      options={[
+        {value: 5, name: '5'}, 
+        {value: 10, name: '10'}, 
+        {value: 25, name: '25'}, 
+        {value: -1, name: 'Показать все'}]}
+      />
       {postError && 
       <h1 style={{textAlign: 'center'}}>Произошла ошибка ${postError}</h1>
       }
-      {isPostLoading
-      ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50, fontSize: '20px', fontWeight: "700", color: "teal"}}>Is Loading<Loader/></div>
-      : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
+      <div ref={lastElement} style={{height: 20, background: 'rgb(22, 22, 22)'}}></div>
+      {isPostLoading &&
+       <div style={{display: 'flex', justifyContent: 'center', marginTop: 50, fontSize: '20px', fontWeight: "700", color: "teal"}}>Is Loading<Loader/></div>    
       }
       <Pagination page={page} changePage={changePage} totalPages={totalPages}></Pagination>
     </div>
